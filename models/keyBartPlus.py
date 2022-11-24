93f63560a32a195c4059ab582f79968821ce77d1
Tao Xiang <xiang.tao@outlook.de>
2022-11-24 21:59:48 +0100

    1

--- models/keyBartPlus.py
+++ models/keyBartPlus.py
@@ -56,6 +56,34 @@ class KeyBartAdapter(nn.Module,GenerationMixin, PyTorchModelHubMixin):
         for i in keyBart.lm_head.parameters():
             i.requires_grad = False
 
+    def prepare_inputs_for_generation(
+            self,
+            decoder_input_ids,
+            past=None,
+            attention_mask=None,
+            head_mask=None,
+            decoder_head_mask=None,
+            cross_attn_head_mask=None,
+            use_cache=None,
+            encoder_outputs=None,
+            **kwargs
+    ):
+        # cut decoder_input_ids if past is used
+        if past is not None:
+            decoder_input_ids = decoder_input_ids[:, -1:]
+
+        return {
+            "input_ids": None,  # encoder_outputs is defined. input_ids not needed
+            "encoder_outputs": encoder_outputs,
+            "past_key_values": past,
+            "decoder_input_ids": decoder_input_ids,
+            "attention_mask": attention_mask,
+            "head_mask": head_mask,
+            "decoder_head_mask": decoder_head_mask,
+            "cross_attn_head_mask": cross_attn_head_mask,
+            "use_cache": use_cache,  # change this to avoid caching (presumably for debugging)
+        }
+
     @add_start_docstrings_to_model_forward(BART_INPUTS_DOCSTRING)
     @replace_return_docstrings(output_type=Seq2SeqLMOutput, config_class=_CONFIG_FOR_DOC)
     @add_end_docstrings(BART_GENERATION_EXAMPLE)
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["torch",'transformers','datasets']
 
 setup(
     name="KeyBartAdapter",
-    version="0.1.3",
+    version="0.1.4",
     author="Tao Xiang",
     author_email="tao.xiang@tum.de",
     description="A package for KeyBartAdapter",
