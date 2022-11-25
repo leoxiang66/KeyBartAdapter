a686990c47ee51e99f5a3caaf5373247dde7cd91
Tao Xiang <xiang.tao@outlook.de>
2022-11-25 10:16:55 +0100

    1

--- models/keyBartPlus.py
+++ models/keyBartPlus.py
@@ -1,14 +1,16 @@
+from pathlib import Path
 from typing import Optional, List, Union, Tuple
 import torch
 import torch.nn as nn
 import random
+import tempfile
+import os
 from torch.nn import CrossEntropyLoss
-from transformers.generation_utils import GenerationMixin
 
 from transformers.utils import (
 add_start_docstrings_to_model_forward,
 add_end_docstrings,
-replace_return_docstrings
+replace_return_docstrings,
 )
 
 from transformers import AutoModelForSeq2SeqLM
@@ -16,7 +18,6 @@ from transformers.models.bart.modeling_bart import (
     BartForConditionalGeneration,
     _expand_mask, logger,
     shift_tokens_right,
-    BartPretrainedModel,
     BART_INPUTS_DOCSTRING,
     _CONFIG_FOR_DOC,
     BART_GENERATION_EXAMPLE,
@@ -32,7 +33,10 @@ from transformers.modeling_outputs import (
     Seq2SeqLMOutput
 )
 
-from huggingface_hub import PyTorchModelHubMixin
+# from huggingface_hub import PyTorchModelHubMixin
+from huggingface_hub.utils import validate_hf_hub_args,HfFolder
+from huggingface_hub.hf_api import HfApi
+from huggingface_hub.repository import Repository
 
 
 class KeyBartAdapter(BartForConditionalGeneration):
@@ -46,7 +50,7 @@ class KeyBartAdapter(BartForConditionalGeneration):
         self.lm_head = keyBart.lm_head
         self.model = BartPlus(keyBart, adapter_hid_dim)
         self.register_buffer("final_logits_bias", torch.zeros((1, self.model.shared.num_embeddings)))
-        # self.config = keyBart.config
+        self.config = keyBart.config
 
 
 
@@ -56,33 +60,160 @@ class KeyBartAdapter(BartForConditionalGeneration):
         for i in keyBart.lm_head.parameters():
             i.requires_grad = False
 
-    def prepare_inputs_for_generation(
+    @validate_hf_hub_args
+    def push_to_hub(
             self,
-            decoder_input_ids,
-            past=None,
-            attention_mask=None,
-            head_mask=None,
-            decoder_head_mask=None,
-            cross_attn_head_mask=None,
-            use_cache=None,
-            encoder_outputs=None,
-            **kwargs
-    ):
-        # cut decoder_input_ids if past is used
-        if past is not None:
-            decoder_input_ids = decoder_input_ids[:, -1:]
+            # NOTE: deprecated signature that will change in 0.12
+            *,
+            repo_path_or_name: Optional[str] = None,
+            repo_url: Optional[str] = None,
+            commit_message: str = "Add model",
+            organization: Optional[str] = None,
+            private: bool = False,
+            api_endpoint: Optional[str] = None,
+            token: Optional[str] = None,
+            git_user: Optional[str] = None,
+            git_email: Optional[str] = None,
+            config: Optional[dict] = None,
+            skip_lfs_files: bool = False,
+            # NOTE: New arguments since 0.9
+            repo_id: Optional[str] = None,  # optional only until 0.12
+            branch: Optional[str] = None,
+            create_pr: Optional[bool] = None,
+            allow_patterns: Optional[Union[List[str], str]] = None,
+            ignore_patterns: Optional[Union[List[str], str]] = None,
+            # TODO (release 0.12): signature must be the following
+            # repo_id: str,
+            # *,
+            # commit_message: str = "Add model",
+            # private: bool = False,
+            # api_endpoint: Optional[str] = None,
+            # token: Optional[str] = None,
+            # branch: Optional[str] = None,
+            # create_pr: Optional[bool] = None,
+            # config: Optional[dict] = None,
+            # allow_patterns: Optional[Union[List[str], str]] = None,
+            # ignore_patterns: Optional[Union[List[str], str]] = None,
+    ) -> str:
+        """
+        Upload model checkpoint to the Hub.
 
-        return {
-            "input_ids": None,  # encoder_outputs is defined. input_ids not needed
-            "encoder_outputs": encoder_outputs,
-            "past_key_values": past,
-            "decoder_input_ids": decoder_input_ids,
-            "attention_mask": attention_mask,
-            "head_mask": head_mask,
-            "decoder_head_mask": decoder_head_mask,
-            "cross_attn_head_mask": cross_attn_head_mask,
-            "use_cache": use_cache,  # change this to avoid caching (presumably for debugging)
-        }
+        Use `allow_patterns` and `ignore_patterns` to precisely filter which files
+        should be pushed to the hub. See [`upload_folder`] reference for more details.
+
+        Parameters:
+            repo_id (`str`, *optional*):
+                Repository name to which push.
+            commit_message (`str`, *optional*):
+                Message to commit while pushing.
+            private (`bool`, *optional*, defaults to `False`):
+                Whether the repository created should be private.
+            api_endpoint (`str`, *optional*):
+                The API endpoint to use when pushing the model to the hub.
+            token (`str`, *optional*):
+                The token to use as HTTP bearer authorization for remote files.
+                If not set, will use the token set when logging in with
+                `transformers-cli login` (stored in `~/.huggingface`).
+            branch (`str`, *optional*):
+                The git branch on which to push the model. This defaults to
+                the default branch as specified in your repository, which
+                defaults to `"main"`.
+            create_pr (`boolean`, *optional*):
+                Whether or not to create a Pull Request from `branch` with that commit.
+                Defaults to `False`.
+            config (`dict`, *optional*):
+                Configuration object to be saved alongside the model weights.
+            allow_patterns (`List[str]` or `str`, *optional*):
+                If provided, only files matching at least one pattern are pushed.
+            ignore_patterns (`List[str]` or `str`, *optional*):
+                If provided, files matching any of the patterns are not pushed.
+
+        Returns:
+            The url of the commit of your model in the given repository.
+        """
+        # If the repo id is set, it means we use the new version using HTTP endpoint
+        # (introduced in v0.9).
+        if repo_id is not None:
+            api = HfApi(endpoint=api_endpoint)
+            api.create_repo(
+                repo_id=repo_id,
+                repo_type="model",
+                token=token,
+                private=private,
+                exist_ok=True,
+            )
+
+            # Push the files to the repo in a single commit
+            with tempfile.TemporaryDirectory() as tmp:
+                saved_path = Path(tmp) / repo_id
+                self.save_pretrained(saved_path, config=config)
+                return api.upload_folder(
+                    repo_id=repo_id,
+                    repo_type="model",
+                    token=token,
+                    folder_path=saved_path,
+                    commit_message=commit_message,
+                    revision=branch,
+                    create_pr=create_pr,
+                    allow_patterns=allow_patterns,
+                    ignore_patterns=ignore_patterns,
+                )
+
+        # If the repo id is None, it means we use the deprecated version using Git
+        # TODO: remove code between here and `return repo.git_push()` in release 0.12
+        if repo_path_or_name is None and repo_url is None:
+            raise ValueError(
+                "You need to specify a `repo_path_or_name` or a `repo_url`."
+            )
+
+        if token is None and repo_url is None:
+            token = HfFolder.get_token()
+            if token is None:
+                raise ValueError(
+                    "You must login to the Hugging Face hub on this computer by typing"
+                    " `huggingface-cli login` and entering your credentials to use"
+                    " `token=True`. Alternatively, you can pass your own token"
+                    " as the `token` argument."
+                )
+        elif isinstance(token, str):
+            token = token
+        else:
+            token = None
+
+        if repo_path_or_name is None:
+            assert repo_url is not None, "A `None` repo URL would have raised above"
+            repo_path_or_name = repo_url.split("/")[-1]
+
+        # If no URL is passed and there's no path to a directory containing files, create a repo
+        if repo_url is None and not os.path.exists(repo_path_or_name):
+            repo_id = Path(repo_path_or_name).name
+            if organization:
+                repo_id = f"{organization}/{repo_id}"
+            repo_url = HfApi(endpoint=api_endpoint).create_repo(
+                repo_id=repo_id,
+                token=token,
+                private=private,
+                repo_type=None,
+                exist_ok=True,
+            )
+
+        repo = Repository(
+            repo_path_or_name,
+            clone_from=repo_url,
+            token=token,
+            git_user=git_user,
+            git_email=git_email,
+            skip_lfs_files=skip_lfs_files,
+        )
+        repo.git_pull(rebase=True)
+
+        # Save the files in the cloned repo
+        self.save_pretrained(repo_path_or_name, config=config)
+
+        # Commit and push!
+        repo.git_add(auto_lfs_track=True)
+        repo.git_commit(commit_message)
+        return repo.git_push()
 
     @add_start_docstrings_to_model_forward(BART_INPUTS_DOCSTRING)
     @replace_return_docstrings(output_type=Seq2SeqLMOutput, config_class=_CONFIG_FOR_DOC)
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = ["torch",'transformers','datasets']
 
 setup(
     name="KeyBartAdapter",
-    version="0.1.8",
+    version="0.1.9",
     author="Tao Xiang",
     author_email="tao.xiang@tum.de",
     description="A package for KeyBartAdapter",
